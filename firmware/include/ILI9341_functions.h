62f4c546328b4a99387efc9d88e20eea963942b3
JBalsells <jorgebalsells@gmail.com>
2024-09-09 16:57:28 -0600

    adding channel 2 in code

--- firmware/include/ILI9341_functions.h
+++ firmware/include/ILI9341_functions.h
@@ -5,7 +5,8 @@
     void initializingDisplay();
     void eraseInformation(int row, int color=0);
     void drawHistogram(const float* data, int numBars);
-    void setNumericValue(long value1, char unit[6], int channel);
+    void setChannelValue(long value1, char unit[6], int channel);
+    void setRelationValue(float value1);
     void drawInformation(const char text[100], int row, int color);
 
 #endif
