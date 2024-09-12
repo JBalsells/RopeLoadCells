6c1e5f116bf397d97dbd014992cf0edcfde334c7
JBalsells <jorgebalsells@gmail.com>
2024-09-11 19:00:51 -0600

    normalize values

--- firmware/include/math_functions.h
+++ firmware/include/math_functions.h
@@ -3,5 +3,6 @@
 
     double random_normal_generator(double media, double desviacion);
     std::vector<int> normalize_vector(const std::vector<int>& vec, int new_min = -50, int new_max = 50);
+    double load_scale(long adc_value=0);
 
 #endif
